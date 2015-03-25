522d71b9a8d3f253764027f396d04607ff35d7ed
florian <florian7cherel@gmail.com>
2015-03-25 13:46:39 +0100

    Calulator TDD avec Bonus (Test suivant les valeurs des arguments des différentes méthodes

--- oop/js/Calculator.js
+++ oop/js/Calculator.js
@@ -1,20 +1,32 @@
 var Calculator = function(){
 	this.result = 0;
 
-	this.add   = function(arguments){
-		this.result += arguments;
+	this.add   = function(number){
+		if(arguments.length >2 || isNaN(number))
+			throw "Le paramètre passé à la méthode n'est pas correct";
+		else
+			this.result += number;
 	}
 
-	this.minus = function(arguments){
-		this.result = this.result - arguments;
+	this.minus = function(number){
+		if(arguments.length >2 || isNaN(number))
+			throw "Le paramètre passé à la méthode n'est pas correct";
+		else
+			this.result = this.result - number;
 	}
 
-	this.divide = function(arguments){
-		this.result = this.result / arguments;
+	this.divide = function(number){
+		if(arguments.length >2 || isNaN(number))
+			throw "Le paramètre passé à la méthode n'est pas correct";
+		else
+			this.result = this.result / number;
 	}
 
-	this.multiply = function(arguments){
-		this.result = this.result * arguments;
+	this.multiply = function(number){
+		if(arguments.length >2 || isNaN(number))
+			throw "Le paramètre passé à la méthode n'est pas correct";
+		else
+			this.result = this.result * number;
 	}
 
 }
